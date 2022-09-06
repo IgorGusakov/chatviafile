fd1ea1bdb0799e79340fc486566cda5ebf04ea31
IgorGusakov <igorgusakov.com@gmail.com>
2022-09-06 17:25:11 +0300

    divide one of unit test

--- unit-test/test_file.cpp
+++ unit-test/test_file.cpp
@@ -141,7 +141,7 @@ TEST(UnitTest, CheckOpenFileOversize)
  * When we open a file that does not have a hash, the first thing we do is add a hash to the file,
  * having previously calculated the hash_test hash for the text test_text
  */
-TEST(UnitTest, CheckAddAndReadHash)
+TEST(UnitTest, CheckAddHash)
 {
 //Arrange
   //name file
@@ -164,24 +164,66 @@ TEST(UnitTest, CheckAddAndReadHash)
 //Act
   //calculate and added hash to file
   uint32_t hash_add_to_file = unit_test.hash_read_from_file();
-//  read from file
-  uint32_t hash_read_to_file = unit_test.hash_read_from_file();
 
 //Assert
   //check add to file
   EXPECT_EQ(hash_test, hash_add_to_file);
+  //delete file
+  std::filesystem::remove(name_test_file);
+}
+
+/*
+ * When we open a file that does not have a hash, the first thing we do is add a hash to the file,
+ * having previously calculated the hash_test hash for the text test_text
+ */
+TEST(UnitTest, CheckReadHash)
+{
+//Arrange
+  //name file
+  std::string name_test_file = "unit_test_write.txt";
+  std::ofstream test_file(name_test_file,std::ios::out);
+  std::string test_text = {"<;hash:f0746a71;>\n"
+                           "The UNIX core concepts have remained more-or-less the same\n"
+                           "since Ritchie and Thompson published their CACM paper. The\n"
+                           "process model and the file system model have remained the\n"
+                           "same."};
+  uint32_t hash_test = 0xF0746A71; // hash test text above
+  test_file << test_text;
+  test_file.close();
+  //max size file
+  uint64_t max_size_file = 3000; //size 100 b
+  bool flag_hash = false;
+  //create object
+  WorkWithFile unit_test(name_test_file,max_size_file,flag_hash);
+  unit_test.OpenFile();
+
+//Act
+//  read from file
+  uint32_t hash_read_to_file = unit_test.hash_read_from_file();
+
+//Assert
   //check read hash
   EXPECT_EQ(hash_test, hash_read_to_file);
+  //delete file
+  std::filesystem::remove(name_test_file);
 }
 /*
  * After we check private method WorkWithFile::check_hash()
- *
  */
 TEST(UnitTest, CheckHash)
 {
 //Arrange
   //name file
   std::string name_test_file = "unit_test_write.txt";
+  std::ofstream test_file(name_test_file,std::ios::out);
+  std::string test_text = {"<;hash:f0746a71;>\n"
+                           "The UNIX core concepts have remained more-or-less the same\n"
+                           "since Ritchie and Thompson published their CACM paper. The\n"
+                           "process model and the file system model have remained the\n"
+                           "same."};
+  uint32_t hash_test = 0xF0746A71; // hash test text above
+  test_file << test_text;
+  test_file.close();
   //max size file
   uint64_t max_size_file = 3000; //size 100 b
   bool flag_hash = false;
